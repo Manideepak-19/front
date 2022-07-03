52d3d2589159f0be02a5d4ffe2267933ebf22c7f
Manideepak Adupa <manideepakadupa19@gmail.com>
2022-07-03 14:48:50 +0530

    commit

---
+++ hello/SocialMedia/app.js
@@ -0,0 +1,35 @@
+// b6wjeDG0KvxdsXwu
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to MongoDB')
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('error connecting',err)
+})
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT);
+})
+
+
+
---
+++ hello/SocialMedia/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Jayakar:b6wjeDG0KvxdsXwu@cluster0.t1cc4.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"bbjamjnrsr2025416198"
+}
